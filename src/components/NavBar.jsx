8adf2f48e86b809a735f45324be9c134bdc17be7
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-11-29 15:59:33 +1100

    Add MenuContext, NavBar with MenuContainer scaffold list

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import styled from "styled-components";
 import logo from "../img/tane-logo.png";
+import { useMenuContext } from "../utils/MenuContext";
 
 const NavContainer = styled.nav`
   display: flex;
   justify-content: center;
-  /* background-color: blue; */
   width: 100%;
   height: 4rem;
   position: fixed;
@@ -13,25 +14,50 @@ const NavContainer = styled.nav`
 `;
 
 const Title = styled.h1`
-  margin: 0;
   position: fixed;
+  margin: 0;
   color: #2f2f2f;
   text-align: center;
   font-family: "Karla", sans-serif;
   font-size: 3rem;
 `;
 
-// const Logo = styled.img`
-//   height: 6rem;
-// `;
+const MenuButton = styled.button`
+  background-color: rgba(255, 255, 255, 0);
+  border: none;
+  font-size: 3rem;
+  position: absolute;
+  align-self: center;
+  left: 1rem;
+`;
+
+const MenuContainer = styled.ul`
+  position: fixed;
+  background-color: white;
+`;
 
 export const NavBar = () => {
+  const { menuVisible, setMenuVisible } = useMenuContext();
+  const toggleMenu = () => {
+    menuVisible ? setMenuVisible(false) : setMenuVisible(true);
+  };
+
   return (
-    <NavContainer>
-      <Title>
-        <i>Tāne</i>
-      </Title>
-      {/* <Logo src={logo} alt="Hello" /> */}
-    </NavContainer>
+    <>
+      <NavContainer>
+        <MenuButton onClick={toggleMenu}>X</MenuButton>
+        <Title>
+          <i>Tāne</i>
+        </Title>
+      </NavContainer>
+      {menuVisible ? (
+        <MenuContainer>
+          <li>Hello</li>
+          <li>Hello</li>
+          <li>Hello</li>
+          <li>Hello</li>
+        </MenuContainer>
+      ) : null}
+    </>
   );
 };
---
+++ src/pages/About.jsx
@@ -0,0 +1,53 @@
+export const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper
+        ornare felis, ac efficitur ante rutrum ac. Proin viverra et est at
+        euismod. Etiam a justo euismod, efficitur lorem quis, finibus neque.
+        Integer vel pulvinar lectus. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Aliquam arcu purus, scelerisque id lacinia ac,
+        imperdiet vel nisi. Sed imperdiet ipsum elit, in maximus sem luctus in.
+        Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam a
+        nulla ante. Etiam quis justo nec lorem varius maximus eu nec nisl. Sed
+        velit lectus, fermentum ut nunc at, ornare varius tortor. Curabitur
+        pretium nisi at facilisis efficitur. Donec tempus laoreet tellus,
+        convallis molestie dui tempus eu. Nunc imperdiet et ligula non blandit.
+        Proin risus sem, laoreet et ultricies sed, convallis vel nulla.
+        Suspendisse et mi vel leo sagittis posuere. Aenean ligula erat,
+        elementum ac tincidunt at, interdum a est. Phasellus molestie accumsan
+        convallis. Etiam molestie nisl sed placerat lacinia. Maecenas et elit
+        libero. Cras nec aliquet ante, eget commodo metus. Nam ut fermentum
+        mauris, et blandit augue. Pellentesque auctor, lectus ut commodo
+        lobortis, ante ipsum feugiat erat, eu posuere sem ante vel ex. Donec
+        lorem libero, luctus non euismod at, ornare in ipsum. Maecenas fermentum
+        commodo fermentum. Cras lobortis tellus diam, vel interdum ex hendrerit
+        non. Nam at ligula tristique, lobortis ex at, venenatis lorem.
+        Vestibulum nec risus imperdiet, faucibus magna et, consectetur augue.
+        Maecenas luctus interdum nisi, nec molestie turpis. Curabitur eu tortor
+        elementum justo dignissim volutpat nec eget tortor. Curabitur placerat
+        erat quis enim interdum molestie. Praesent vitae ipsum porttitor, rutrum
+        leo vitae, laoreet magna. Maecenas a magna posuere lorem gravida
+        scelerisque. Vivamus ornare pulvinar tortor ut ultricies. Pellentesque
+        faucibus nec libero eget porttitor. Nunc placerat facilisis eros, at
+        cursus augue egestas vel. Suspendisse potenti. Donec ut vulputate ipsum.
+        Pellentesque molestie, neque at ultrices bibendum, velit nisi congue
+        ligula, vel accumsan augue erat quis orci. Curabitur maximus sapien a
+        tortor suscipit congue. In tincidunt dapibus interdum. Nam elementum
+        quam vitae elit vulputate, in tempor nibh suscipit. Donec non suscipit
+        neque, vel ultricies velit. Duis vitae ultrices risus. Nulla et dolor
+        eget erat scelerisque pretium. Ut quis dictum libero. Curabitur id
+        vestibulum sem. Cras pellentesque accumsan tempus. Fusce libero velit,
+        luctus et suscipit nec, vestibulum sit amet risus. Etiam sed odio
+        ligula. Nulla lobortis, diam a malesuada lobortis, ex magna aliquet est,
+        nec sagittis mauris diam nec metus. Vivamus a tortor iaculis mi auctor
+        semper. Proin semper ligula ut urna vestibulum, at porttitor purus
+        consectetur. Cras sed malesuada urna. Proin euismod pretium ante, nec
+        porta sem laoreet quis. Vivamus lobortis nunc eu ipsum ultrices, at
+        fermentum arcu accumsan. Pellentesque sed urna elit. Duis accumsan neque
+        id diam bibendum volutpat.
+      </p>
+    </>
+  );
+};
