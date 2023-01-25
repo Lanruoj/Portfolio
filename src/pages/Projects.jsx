da0a993b422efd68c9b91bc5822f7106ac4f38e6
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-25 15:15:18 +1100

    Create shell <Project> component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,11 @@
+import { Project } from "../components/Project";
 import { Header } from "../components/Header";
 
 export const Projects = () => {
   return (
     <>
       <Header>Projects</Header>
+      <Project />
     </>
   );
 };
