902edd9fe269921ca8cc11e58789fd1bff922fda
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-25 15:23:26 +1100

    Create basic <Project> styling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,3 +1,26 @@
+import styled from "styled-components";
+import { Header } from "./Header";
+
+const ProjectContainer = styled.article`
+  background-color: red;
+  border-radius: 1rem;
+  padding: 1rem;
+`;
+
+const ProjectImage = styled.img`
+  max-width: 90%;
+`;
+
 export const Project = (props) => {
-  return <p>Hello</p>;
+  return (
+    <ProjectContainer>
+      <h3>Portfolio</h3>
+      <p>
+        uifsdgjh nuijroeinfh jrojnfg dfuhigewn hreguirejhfn iuehguidh rgbduiorj
+        hfiehgu fbwiuegheruifbiqw ujhfbwdj iubfu iwebrfsadhdfs yiuhiyughfuie
+        buifv
+      </p>
+      <ProjectImage src={require("../img/portrait-cropped.jpeg")} />
+    </ProjectContainer>
+  );
 };
