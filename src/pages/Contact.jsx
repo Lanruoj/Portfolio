13c248343fc8f152135edaabe4dcd64fbd52afe2
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-26 11:27:56 +1100

    Add basic email/phone links to

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,22 @@
+import styled from "styled-components";
+
+const ContactDetails = styled.address`
+  font-style: normal;
+`;
+
+const ContactLink = styled.a`
+  text-decoration: none;
+`;
+
 export const Contact = () => {
-  return <p>HELLO</p>;
+  return (
+    <ContactDetails>
+      <h3>Email</h3>
+      <ContactLink href="mailto:tanesamuelkaio@gmail.com">
+        > tanesamuelkaio@gmail.com
+      </ContactLink>
+      <h3>Phone</h3>
+      <ContactLink href="tel:+61466651820">> 0466651820</ContactLink>
+    </ContactDetails>
+  );
 };
