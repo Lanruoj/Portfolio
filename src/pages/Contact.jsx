f0d403528aadf58e19258e56a7bc716c660217c3
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-26 12:24:39 +1100

    Create basic email contact form with EmailJS

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,10 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import styled from "styled-components";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const ContactDetails = styled.address`
+const ContactDetails = styled.div`
   font-style: normal;
 `;
 
@@ -9,14 +13,44 @@ const ContactLink = styled.a`
 `;
 
 export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_blxaonb",
+        "template_xwduzni",
+        form.current,
+        "CcsmHVdEk7eIWa1pW"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <ContactDetails>
-      <h3>Email</h3>
+      {/* <h3>Email</h3>
       <ContactLink href="mailto:tanesamuelkaio@gmail.com">
         > tanesamuelkaio@gmail.com
       </ContactLink>
       <h3>Phone</h3>
-      <ContactLink href="tel:+61466651820">> 0466651820</ContactLink>
+      <ContactLink href="tel:+61466651820">> 0466651820</ContactLink> */}
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     </ContactDetails>
   );
 };
