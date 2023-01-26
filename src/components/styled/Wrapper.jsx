66fd7cc857f7ceaba77064f8f65969f7a47c39a5
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-01-26 17:25:56 +1100

    Use windowSize context for <Wrapper> max-height

--- src/components/styled/Wrapper.jsx
+++ src/components/styled/Wrapper.jsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
+import { useWindowSizeContext } from "../../utils/WindowSizeContext";
 
 const StyledWrapper = styled.section`
   display: flex;
   flex-direction: column;
-  max-height: 50vh;
+  justify-content: center;
+  max-height: ${(props) => (props.windowSize > 800 ? "50vh" : "100%")};
   flex-wrap: wrap;
   align-items: center;
 `;
 
 export const Wrapper = ({ children }) => {
-  return <StyledWrapper>{children}</StyledWrapper>;
+  const { windowSize } = useWindowSizeContext();
+  return <StyledWrapper windowSize={windowSize}>{children}</StyledWrapper>;
 };
