c7f86433f6f12bd163d9a34163f1c804761b74bf
Tane Kaio <tanesamuelkaio@gmail.com>
2023-04-19 11:16:27 +1000

    Update `<About>` information

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,20 +12,31 @@ export const About = () => {
         <Portrait />
         <article>
           <LargeParagraph>
-            Hello!
-            <br /> I'm Tāne and I'm a 27 year old junior web developer currently
-            based in the lush surroundings of the foothills of the Dandenong
-            Ranges in Melbourne, Victoria. As a student of Web Development at
-            Coder Academy I spend most of my time learning and practising code,
-            sharpening my problem solving skills and learning more about the
-            world of technology. My adventure into the land of coding may not
-            have started long ago, however I've already learned an immense
-            amount of valuable practical lessons and industry specific knowledge
-            which I can't wait to apply to the real world.
+            Hello! I'm Tāne and I'm a 27 year old Māori Australian software
+            engineer currently based in the lush surroundings of the foothills
+            of the Dandenong Ranges in Melbourne, Victoria.
+            <br />
+            <br />
+            Currently participating in an internship program with flagship
+            whitegoods e-commerce company Winning Group, I am eagerly awaiting
+            an opportunity to take the next step in my professional development
+            and offer my skills, attitude and personality to the technology
+            industry.
+            <br />
+            <br />
+            As a recent graduate of Web Development at Coder Academy I spend
+            most of my time learning and practising code, sharpening my problem
+            solving skills and learning more about the world of technology.
+            <br />
+            <br />
+            My adventure into the land of coding may not have started long ago,
+            however I've already learned an immense amount of valuable practical
+            lessons and industry specific knowledge which I can't wait to apply
+            to the real world.
           </LargeParagraph>
-          <LocalLink to={"/projects"} text={"Projects"} />
         </article>
       </Wrapper>
+      <LocalLink to={"/projects"} text={"Projects"} />
     </>
   );
 };
