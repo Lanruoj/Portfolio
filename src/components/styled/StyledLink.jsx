6ccbd29ace809c50abd2aa3fa2451f268dce0669
Tane Kaio <tanesamuelkaio@gmail.com>
2023-05-03 13:09:45 +1000

    Add square border to `<StyledLink>`

--- src/components/styled/StyledLink.jsx
+++ src/components/styled/StyledLink.jsx
@@ -4,13 +4,14 @@ import { Link } from "react-router-dom";
 const StyledLink = styled(Link)`
   display: inline-block;
   position: relative;
-  right: 1rem;
   padding: 1rem;
   font-family: "Spline Sans Mono", sans-serif;
   font-size: 1.2rem;
   color: black;
   text-align: center;
-  text-decoration: underline;
+  text-decoration: none;
+  border: solid black 1px;
+  line-height: 0.5;
   ::before {
     content: ">\u00A0";
     display: inline-block;
