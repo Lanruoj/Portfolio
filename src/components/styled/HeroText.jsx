3890cc22289c92529b6f48bc05133ff7f5fb9e05
Tane Kaio <tanesamuelkaio@gmail.com>
2023-05-17 11:16:58 +1000

    Fix text centering

--- src/components/styled/HeroText.jsx
+++ src/components/styled/HeroText.jsx
@@ -5,8 +5,7 @@ export const HeroText = (props) => {
 };
 
 const Text = styled.h1`
-  text-align: left;
-  padding-left: 15rem;
+  text-align: center;
   font-size: 15vw;
   font-family: "Spline Sans Mono", monospace;
   color: #2d2d2d;
